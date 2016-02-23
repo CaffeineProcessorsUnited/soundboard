c4629cab8f2d627fff8a437b44b607bfd72232cc
Malte Heinzelmann <maltehd@gmail.com>
2016-02-23 16:09:04 +0100

    Sample socket.io implementation

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var web = express();
+var server = require('http').Server(web);
+var io = require('socket.io')(server);
+var ioc = require('socket.io-client');
+
+web.use(express.static('public'));
+
+server.listen(8080, function () {
+	console.log('Web interface is running on port 8080!');
+	var api = express();
+	var bodyParser = require("body-parser");
+	api.use(bodyParser.urlencoded({ extended: false }));
+	api.use(bodyParser.json());
+	var client;
+	
+	api.get('*', function(req, res) {
+		res.send('This api doesn\'t wirk through GET. Please switch to POST!');
+	});
+
+	api.post('*', function(req, res) {
+		client.emit('post', request.body);
+		res.send('Not specified');
+	});
+
+	api.listen(3000, function () {
+		console.log('Api is running on port 3000!');
+		client = ioc('http://localhost:8080');
+	});
+});
+
+io.on('connection', function(socket) {
+	console.log('Client connected');
+	socket.on('test', function() {
+		console.log('Someone usccessfully tested something!');
+	});
+});
+
+// api is used as a connection between POST requests and socket
+
+

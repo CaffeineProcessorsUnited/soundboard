eea2a0e3c8a108a184f80b1d1e2d15f72432c172
Malte Heinzelmann <malte@hnzlmnn.de>
2016-05-11 21:07:28 +0200

    Added fileviewer capability

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const util = require('util');
 const crypto = require('crypto');
 const fs = require('fs');
+const path = require('path');
 
 var callerId = require('caller-id');
 
@@ -39,7 +40,7 @@ var Class = function(methods) {
   for (var property in methods) {
     klass.prototype[property] = methods[property];
   }
-  if (!klass.prototype.initialize) klass.prototype.initialize = function(){};
+  if (!klass.prototype.initialize) klass.prototype.initialize = function() {};
   return klass;
 };
 
@@ -74,10 +75,10 @@ var classes = {
       }
     },
 		clear: function() {
-      while(this.queue.length > 0) {
+      while (this.queue.length > 0) {
         this.queue.delete(this.queue.length - 1);
       }
-      this.currentPos = 0;
+      this.currentPos = -1;
 		},
     getCurrentTrack: function() {
       return this.queue[this.currentPos];
@@ -87,13 +88,12 @@ var classes = {
     },
     next: function(position) {
       if (!!position) {
-        this.currentPos = postion;
+        this.currentPos = position;
       } else {
-        if(this.isShuffel) {
+        if (this.isShuffel) {
           this.currentPos = randomInt(0, this.queue.length);
-        }
-        else {
-          this.currentPos = (this.currentPos +1)%this.queue.length;
+        } else {
+          this.currentPos = (this.currentPos + 1) % this.queue.length;
         }
       }
     },
@@ -103,19 +103,19 @@ var classes = {
     getCurrentPosition: function() {
       return this.currentPos;
     },
-		loadQueueFromPlaylist: function(name){
-			if(runtime.playlists[name]){
+		loadQueueFromPlaylist: function(name) {
+			if (runtime.playlists[name]) {
 				this.clear();
-				runtime.playlists[name].tracks.forEach(function(trackinfo){
+				runtime.playlists[name].tracks.forEach(function(trackinfo) {
 					this.add(new classes.Track(trackinfo.service, trackinfo.path));
 				});
 			}
 		},
-		saveQueueAsPlaylist: function(name){
+		saveQueueAsPlaylist: function(name) {
 			runtime.playlists[name]={
 				tracks: []
 			}
-			this.queue.forEach(function(track){
+			this.queue.forEach(function(track) {
 				var trackinfo={
 					service: track.getService(),
 					path:	track.getPath()
@@ -186,10 +186,38 @@ var runtime = new (function(undefined) {
     this.userLoggedin = function(socket) {
       return (!!this.socketdata(socket).user && !!this.socketdata(socket).user.authenticated);
     };
+    this.readdir = function(base) {
+      self = this;
+      base = path.resolve('./', base);
+      var list = {};
+      try {
+        var stats = fs.lstatSync(base)
+        if (!!stats) {
+          if (stats.isDirectory()) {
+            var dirs = fs.readdirSync(base);
+            if (!!dirs) {
+              dirs.forEach(function(dir) {
+                var file = path.relative('./', path.resolve(base, dir));
+                list[dir] = self.readdir('./' + file);
+              });
+            }
+          } else if (stats.isFile()) {
+            var file = path.relative('./public/songs/filesystem/', path.resolve(base));
+            return file;
+          }
+        }
+      } catch(e) {
+        console.log("Couldn't read files" + e);
+      }
+      return list;
+    }
+    this.getFiles = function() {
+      return this.readdir('./public/songs/filesystem/');
+    }
 })();
 
 server.listen(8080, function() {
-  runtime.log('Web interface is running on port 8080!');
+  runtime.log('web player is running on port 8080!');
   // api is used as a connection between POST requests and socket
   var api = express();
   var bodyParser = require("body-parser");
@@ -212,12 +240,13 @@ server.listen(8080, function() {
   api.listen(3000, function() {
     runtime.log('Api is running on port 3000!');
     client = ioc('http://localhost:8080');
+    client.emit("getFiles");
   });
 });
 
 io.on('connection', function ioOnConnection(socket) {
   runtime.log('Client connected');
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function() {
     console.log('Client disconnected');
   });
   socket.on('test', function() {
@@ -241,18 +270,27 @@ io.on('connection', function ioOnConnection(socket) {
       //return;
     }
     runtime.log(data);
-    if (data["service"] && data['path']){
+    if (data["service"] && data['path']) {
       var track = new classes.Track(data['service'],data['path']);
       if (track.service && track.path) {
-        if(data["next"]){
-          runtime.queue.add(new classes.Track(track.service, track.path, track.time || undefined),runtime.queue.getCurrentPosition()+1);
+        if (track.service == "filesystem") {
+          if (!path.resolve('./public/songs', track.path).startsWith(path.resolve('./public/songs') + '/')) {
+            runtime.log('Invalid file path. The file must be inside the songs directory!');
+          }
+        }
+        if (track.service == "url") {
+          if (!track.path.startsWith('http')) {
+            runtime.log('Invalid url path. The file must be a http ot https url!');
+          }
+        }
+        if(data["next"]) {
+          runtime.queue.add(new classes.Track(track.service, track.path, track.time || undefined),runtime.queue.getCurrentPosition() + 1);
           runtime.queue.next(runtime.queue.getCurrentPosition() + 1);
           io.sockets.emit('poll');
-        }
-        else {
+        } else {
           runtime.queue.add(new classes.Track(track.service, track.path, track.time || undefined));
-          io.to(socket.id).emit("poll");
         }
+        io.to(socket.id).emit("poll");
       } else {
         runtime.log('This doesn\'t seem to be a valid track.');
       }
@@ -282,7 +320,7 @@ io.on('connection', function ioOnConnection(socket) {
     io.to(socket.id).emit('get_queue', { "queue": runtime.queue.list(), "currentTrack": runtime.queue.getCurrentTrack()});
   });
 	socket.on('get_playlist', function(data) {
-		if(data.name){
+		if(data.name) {
 			io.to(socket.id).emit('get_playlist', { "tracks": runtime.playlists[data.name].tracks});
 		} else {
 			var playlistnames = [];
@@ -299,38 +337,41 @@ io.on('connection', function ioOnConnection(socket) {
   socket.on('get_current_track', function socketCurrentTrack() {
     io.to(socket.id).emit('get_current_track', {'currentTrack': runtime.queue.getCurrentTrack()/*||new classes.Track("youtube","jHPOzQzk9Qo"/*"filesystem","epicsaxguy.wav")*/, 'time': runtime.playback_time, 'playing':runtime.playing});
   });
-  socket.on('next',function socketNextElement(){
+  socket.on('next',function socketNextElement() {
     runtime.queue.next();
     runtime.playback_time=0;
     runtime.playing = true;
     io.sockets.emit("poll");
   });
-  socket.on('prev', function socketPrevElement(){
+  socket.on('prev', function socketPrevElement() {
     runtime.queue.prev();
     runtime.playback_time=0;
     io.sockets.emit('poll');
   });
-  socket.on('current_Time',function onCurrentTime(data){
+  socket.on('current_Time', function onCurrentTime(data) {
     runtime.log(data["time"]);
     runtime.playback_time=data["time"];
   });
-  socket.on('play',function onPlay(){
+  socket.on('play', function onPlay() {
     runtime.log("Play");
     runtime.playing = true;
     io.sockets.emit("poll");
   });
-  socket.on("pause",function onPause(){
+  socket.on("pause", function onPause() {
     runtime.log("Pause");
     runtime.playing = false;
     io.sockets.emit("get_current_time");
     io.sockets.emit("poll");
   });
-  socket.on("stop",function onStop(){
+  socket.on("stop", function onStop() {
     runtime.playback_time = 0;
     runtime.playing = false;
     io.sockets.emit("poll");
   });
-  socket.on('isPlaying',function onIsPlaying(){
+  socket.on('isPlaying', function onIsPlaying() {
     io.to(socket.id).emit('isPlaying', {'playing': runtime.playing});
+  });
+  socket.on('getFiles', function getFiles() {
+    io.to(socket.id).emit('getFiles', {'files': runtime.getFiles()});
   });
 });

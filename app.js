5e5a5d0f0d0c4d05267c28b1f21db1ee653b329d
Malte Heinzelmann <malte@hnzlmnn.de>
2016-07-09 12:20:18 +0200

    Removed obsolete code from app.js

--- app.js
+++ app.js
@@ -44,17 +44,6 @@ function randomInt(low, high) {
   return Math.floor(Math.random() * (high - low) + low);
 }
 
-var Class = function(methods) {
-  var klass = function() {
-    this.initialize.apply(this, arguments);
-  };
-  for (var property in methods) {
-    klass.prototype[property] = methods[property];
-  }
-  if (!klass.prototype.initialize) klass.prototype.initialize = function() {};
-  return klass;
-};
-
 web.use(express.static('public'));
 
 var classes = {
